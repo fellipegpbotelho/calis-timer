790b4e730adb2206975dfae990e699d0ff281157
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-03-03 22:30:39 -0300

    add font ubuntu to assets; installing and linking react-navigation/react-native-gesture-handler; configuring app navigation;

---
+++ src/screens/EMOMScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const EMOMScreen = props => {
+  return (
+    <View>
+      <Text>EMOMScreen</Text>
+    </View>
+  );
+};
+
+export default EMOMScreen;
